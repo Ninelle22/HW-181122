567d5e7fdb94feacc8b7e6c6be1cf6c3fa871c3b
«Nina <ninelle22@yandex.ru>
2022-11-21 16:49:02 +0200

    ExampleHW4

--- Example 2/ExampleHW4/Program.cs
+++ Example 2/ExampleHW4/Program.cs
@@ -2,16 +2,16 @@
 using static System.Console;
 Clear();
 
-Write ("Введите первое число: ");
+Write ("First number: ");
 int number = int.Parse(ReadLine());
-Write ("Введите второе число: ");
+Write ("Second number: ");
 int number2 = int.Parse(ReadLine());
 
 if (number>number2)
 {
-    WriteLine ($"Число {number} является максимальным, а число {number2} минимальным.");
+    WriteLine ($"Number {number} is maximum, number is {number2} minimum.");
 }
 else
 {
- WriteLine ($"Число {number} является минимальным, а число {number2} максимальным.");
+ WriteLine ($"Number {number} is minimum, number is {number2} maximum.");
 }
