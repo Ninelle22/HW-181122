a7c8ee29bdbddd50605f83fa894a3dfcb0b07471
«Nina <ninelle22@yandex.ru>
2022-11-21 17:09:54 +0200

    Example 6

--- Example 8/Example 6/Program.cs
+++ Example 8/Example 6/Program.cs
@@ -3,8 +3,12 @@ using static System.Console;
 Clear();
 Write ("Введите любое число: ");
 int number = int.Parse(ReadLine());
-int x = number % 2;
-if (x ==0);
-    Write ($"Число {number} является четным");
+
+if (number % 2 == 0)
+{
+    Console.WriteLine ("да");
+}
 else
-Write ($"Число {number} не является четным");
+{
+    Console.WriteLine ("нет");
+}
